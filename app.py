ebd5bf023d7a006371fdbebce410b9c692f573c7
aracope <156398622+aracope@users.noreply.github.com>
2025-05-02 12:52:27 -0700

    Create Cupcake model and seed database with sample cupcakes

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-"""Flask app for Cupcakes"""
+from flask import Flask
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
--- models.py
+++ models.py
@@ -1,1 +1,17 @@
-"""Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def __repr__(self):
+        return f"<Cupcake id={self.id} flavor={self.flavor} size={self.size} rating={self.rating}>"
